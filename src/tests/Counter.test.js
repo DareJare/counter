c3e01fc205bb850730668525b34fbf0f28f69cb5
Jared <janthofer@ufl.edu>
2022-11-30 11:40:13 -0500

    add 4 react tests to counter testing components

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,40 @@
 // import necessary react testing library helpers here
 // import the Counter component here
 
+import {render, screen} from "@testing-library/react";
+import App from "../components/App";
+import userEvent from "@testing-library/user-event";
+
 beforeEach(() => {
   // Render the Counter component here
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+    render(<App />);
+    const counterMessage = screen.getByText(/Counter/i);
+    expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+    render(<App />);
+    const initialCount = screen.getByText(/0/i);
+    expect(initialCount).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+    render(<App />);
+    userEvent.click(screen.getByRole('button', {name: '+'}))
+    const count = screen.getByText(/1/i);
+    expect(count).toBeInTheDocument();
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+    render(<App />);
+    userEvent.click(screen.getByRole('button', {name: '-'}))
+    const count = screen.getByText(/-1/i);
+    expect(count).toBeInTheDocument();
 });
